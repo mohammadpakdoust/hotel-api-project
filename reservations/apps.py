546f808bad0c0416d057d8d7ee22b2c8775cd9f8
Mohammad Pakdoust <mohpakdoust@gmail.com>
2026-03-25 23:08:12 -0300

    Finalized production settings (DEBUG=False) and app configuration

--- reservations/apps.py
+++ reservations/apps.py
@@ -5,20 +5,3 @@ class ReservationsConfig(AppConfig):
     name = 'reservations'
 
 
-from django.contrib.auth import get_user_model
-
-class ReservationsConfig(AppConfig):
-    ...
-
-    def ready(self):
-        User = get_user_model()
-
-        try:
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser(
-                    "admin",
-                    "admin@example.com",
-                    "admin123"
-                )
-        except Exception:
-            pass
