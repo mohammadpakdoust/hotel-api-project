d2d66f9a42af373cc318715d472709cf3a992891
Mohammad Pakdoust <mohpakdoust@gmail.com>
2026-03-26 14:50:31 -0300

    Temporary DEBUG=True for live error debugging

--- hotel_api/settings.py
+++ hotel_api/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-dev-only')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*'] # OR os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
