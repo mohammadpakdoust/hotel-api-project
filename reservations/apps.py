4885bae71ffbe839e54cce33ef1be7ba0c330fb0
Mohammad Pakdoust <mohpakdoust@gmail.com>
2026-03-25 23:02:00 -0300

    fix pass

--- reservations/apps.py
+++ reservations/apps.py
@@ -3,3 +3,22 @@ from django.apps import AppConfig
 
 class ReservationsConfig(AppConfig):
     name = 'reservations'
+
+
+from django.contrib.auth import get_user_model
+
+class ReservationsConfig(AppConfig):
+    ...
+
+    def ready(self):
+        User = get_user_model()
+
+        try:
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    "admin",
+                    "admin@example.com",
+                    "admin123"
+                )
+        except Exception:
+            pass
