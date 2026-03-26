cc3c82e664c2e33a31974b26ca5b34c147129873
Mohammad Pakdoust <mohpakdoust@gmail.com>
2026-03-26 01:01:15 -0300

    Simplified database configuration: used dj-database-url.parse as requested

--- hotel_api/settings.py
+++ hotel_api/settings.py
@@ -85,7 +85,9 @@ WSGI_APPLICATION = 'hotel_api.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
